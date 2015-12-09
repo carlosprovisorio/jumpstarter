5f8066c9d4b6486d5b090714561b717705d117ce
carlosprovisorio <carlosprovisorio@icloud.com>
2015-12-08 19:47:57 -0500

    add actions for update and edit for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+     	redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	private
 
 	def user_params
