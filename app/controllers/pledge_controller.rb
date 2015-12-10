6dde312b7c2f694cfb4bb38262e9c478faae9f29
carlosprovisorio <carlosprovisorio@icloud.com>
2015-12-10 10:25:12 -0500

    Implemented how much money my project has reised

--- app/controllers/pledge_controller.rb
+++ app/controllers/pledge_controller.rb
@@ -1,4 +1,4 @@
-class PledgeController < ApplicationController
+class PledgesController < ApplicationController
 before_action :require_login
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :rewards
-	has_many :pledges, through: :rewards 
+	has_many :pledges, through: :rewards
+
+def amount_raised
+   self.pledges.sum(:amount)
+
+end
+
+
+
 end
