bd1728711500c333379cfe23f384e676c134abe1
BlackDiscGH <avinashjham@yahoo.com>
2015-12-14 13:14:30 -0500

    Added name field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209013722) do
+ActiveRecord::Schema.define(version: 20151212170403) do
 
   create_table "pledges", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "reward_id"
     t.integer  "user_id"
-
   end
 
   create_table "projects", force: :cascade do |t|
@@ -51,6 +50,7 @@ ActiveRecord::Schema.define(version: 20151209013722) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
